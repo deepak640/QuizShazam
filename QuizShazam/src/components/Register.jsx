ce5e1c5ee28ed8df52d7f6860f583d884b1b81e2
deepak640 <ayushdeepnegi@gmail.com>
2024-06-01 11:45:28 +0530

    register page

--- QuizShazam/src/components/Register.jsx
+++ QuizShazam/src/components/Register.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [user, setUser] = useState({
+    Username: "",
+    Email: "",
+    Password: "",
+  });
 
-export default Register
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div className="Login-container">
+      <form onSubmit={handleSubmit}>
+        <h2>Register</h2>
+        <div>
+          <label htmlFor="Username">Username</label>
+          <br />
+          <input
+            type="text"
+            id="Username"
+            name="Username"
+            value={user.Username}
+            placeholder="Enter Username"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="Email">Email</label>
+          <br />
+          <input
+            type="email"
+            id="Email"
+            name="Email"
+            value={user.Email}
+            placeholder="Enter Email"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="Password">Password</label>
+          <br />
+          <input
+            type="password"
+            id="Password"
+            name="Password"
+            value={user.Password}
+            placeholder="Enter Password"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <span>
+          <input type="checkbox" id="checkbox" name="checkAccount" />
+          <label htmlFor="checkbox">Remember me</label>
+        </span>
+        <br />
+        <button type="submit">REGISTER</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
