6f714c2ebf79969469e428d5840397b77a6d6aaf
deepak640 <ayushdeepnegi@gmail.com>
2024-06-20 19:25:09 +0530

    quiz result interface

--- QuizShazam/src/components/Result.jsx
+++ QuizShazam/src/components/Result.jsx
@@ -1,8 +1,44 @@
 import React from "react";
-import withAuth from "../auth/withAuth";
+import "../css/result.css"
 
 const Result = () => {
-  return <div>Result</div>;
+  const questions = [
+    {
+      question: "What is the capital of France?",
+      options: ["Paris", "London", "Madrid", "Berlin"],
+      correctAnswer: "Paris",
+      userAnswer: "London",
+    },
+    {
+      question: "What is the largest ocean in the world?",
+      options: [
+        "Atlantic Ocean",
+        "Pacific Ocean",
+        "Indian Ocean",
+        "Arctic Ocean",
+      ],
+      correctAnswer: "Pacific Ocean",
+      userAnswer: "Atlantic Ocean",
+    },
+  ];
+
+  return (
+    <div className="quiz-results">
+      <h1>Quiz Results</h1>
+      {questions.map((question, index) => (
+        <div className="question" key={index}>
+          <p>{question.question}</p>
+          <ul>
+            {question.options.map((option, optionIndex) => (
+              <li key={optionIndex} className={option === question.correctAnswer? "correct" : option === question.userAnswer? "incorrect" : ""}>
+                {option}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 };
 
-export default withAuth(Result);
+export default Result;
