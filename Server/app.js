233eb4d44bb34b868addd0835bec72f1a7e5e831
deepak640 <ayushdeepnegi@gmail.com>
2024-06-21 09:48:22 +0530

    updated port

--- Server/app.js
+++ Server/app.js
@@ -6,26 +6,25 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-var bodyparser = require("body-parser")
-var cors = require('cors')
+var bodyparser = require("body-parser");
+var cors = require("cors");
 require("dotenv").config();
 var app = express();
 app.disable("etag");
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
-app.use(cors())
+app.use(cors());
 app.use(logger("dev"));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
-
+var port = process.env.PORT || "3000";
 mongoose
   .connect(process.env.MONGO_URI, options)
   .then(() => {
@@ -38,6 +37,9 @@ mongoose
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
+app.listen(port, () => {
+  console.log("listening");
+});
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
