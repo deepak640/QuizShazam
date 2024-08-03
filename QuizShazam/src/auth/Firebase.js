4455df203173621273df98d6800a4cd7be05e787
deepak640 <ayushdeepnegi@gmail.com>
2024-08-04 01:29:28 +0530

    updated sercret files

--- QuizShazam/src/auth/Firebase.js
+++ QuizShazam/src/auth/Firebase.js
@@ -3,19 +3,27 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
+const {
+  VITE_REACT_API_KEY,
+  VITE_REACT_DOMAIN,
+  VITE_REACT_PROJECT_NAME,
+  VITE_REACT_API_BUCKET,
+  VITE_REACT_MESSAGE,
+  VITE_REACT_APPID,
+  VITE_REACT_ID,
+} = import.meta.env;
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAP1Ngd4mZw12g3l33moXsfcnwvU889o1s",
-  authDomain: "quizshazam.firebaseapp.com",
-  projectId: "quizshazam",
-  storageBucket: "quizshazam.appspot.com",
-  messagingSenderId: "984615765846",
-  appId: "1:984615765846:web:98d7ded1570c1bfebff8f7",
-  measurementId: "G-RMBPYL1Q75",
+  apiKey: VITE_REACT_API_KEY,
+  authDomain: VITE_REACT_DOMAIN,
+  projectId: VITE_REACT_PROJECT_NAME,
+  storageBucket: VITE_REACT_API_BUCKET,
+  messagingSenderId: VITE_REACT_MESSAGE,
+  appId: VITE_REACT_APPID,
+  measurementId: VITE_REACT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
+export const auth = getAuth(app);
